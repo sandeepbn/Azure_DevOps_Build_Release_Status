8a9b4aaadba9c6e2d97b64ab1e480fddc3d0b5b6
chiruskywood <chiruskywood@gmail.com>
2019-06-12 15:26:22 +0530

    Add project files.

---
+++ Azure_DevOps_Build_Release_Status/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Azure_DevOps_Build_Release_Status.Global" Language="C#" %>
---
+++ Azure_DevOps_Build_Release_Status/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Azure_DevOps_Build_Release_Status
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
